6326861c98b6a5242be5283947b1cc1fe517354c
Zion <amazion24@outlook.com>
2025-11-07 09:17:21 +0200

    הוספנו קבצי צד לקוח

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 require('dotenv').config();
-const app = express();
 const port = process.env.PORT;
 const api = process.env.HOST;
+const app = express();
+app.use(express.static(__dirname));
 
-
+app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/index.html')})
 
 app.listen(port,()=>{console.log(`http://${api}:${port}`)})
