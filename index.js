4f157e696d37f5e2fff8ca5e87d2e9a5e74c4d9f
Zion <amazion24@outlook.com>
2025-11-07 11:05:20 +0200

    עשינו נקודת קצה שצריכה להחזיר את כל המשתמשים מהדאטהבייס

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const api = process.env.HOST;
 const app = express();
 app.use(express.static(__dirname));
 const db = require('./config/db_config');
+app.use(express.json());
 
 app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/index.html')})
+app.use('/users',require('./routes/users_R'));
 
 app.listen(port,()=>{console.log(`http://${api}:${port}`)})
