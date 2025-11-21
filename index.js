90bdc784ecce7454c5726a26f92e7fef5274be3a
Zion <amazion24@outlook.com>
2025-11-21 11:11:25 +0200

    הוספנו נקודת קצה של הרשמה עם הצפנת הסיסמה

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ app.use(express.json());
 
 app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/index.html')})
 app.use('/users',require('./routes/users_R'));
+app.use('/auth',require('./routes/auth_R'));
 
 app.listen(port,()=>{console.log(`http://${api}:${port}`)})
