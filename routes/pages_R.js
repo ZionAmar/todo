a8cba7744e239411fd8f0a3b745629c3b68487f5
Zion <amazion24@outlook.com>
2025-12-26 09:39:26 +0200

    הוספנו דף לוגין

--- routes/pages_R.js
+++ routes/pages_R.js
@@ -4,5 +4,6 @@ const path = require('path');
 
 router.get('/',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","index.html"))})
 router.get('/reg',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","reg.html"))})
+router.get('/login',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","login.html"))})
 
 module.exports = router;
