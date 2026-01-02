96b5d900efe0654912411e4fe620c60e0b7614c6
Zion <amazion24@outlook.com>
2026-01-02 10:56:40 +0200

    הוספנו מערך קטגוריות בצד הלקוח ששומר את כל הקטגוריות בצורה ממיונת

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,7 @@
 let greating = "Hello ";
 greating += localStorage.getItem('name');
 document.getElementById('greating').innerHTML = greating;
+allCategories = [];
 
 async function getTasks() {
     try {
@@ -20,16 +21,37 @@ async function getTasks() {
     }
 }
 
+async function getCategories() {
+    try {
+        let response = await fetch('/categories');
+        if (response.status == 401) {
+            window.location.href = '/login';
+            return;
+        }
+        let data = await response.json();
+        if (response.status == 400) {
+            alert(data.message);
+            return;
+        }
+        for(let c of data){
+            allCategories[c.id] = c;
+        }
+    } catch (err) {
+        alert(err)
+    }
+}
+
 function createTable(data) {
     let txt = "";
     for (obj of data) {
         if (obj) {
             let isChecked = obj.is_done ? "checked" : "";
             let rowClass = obj.is_done ? "class='rowClass'" : "";
+            let catName = allCategories[obj.category_id]?allCategories[obj.category_id].name:'--';
             txt += `<tr ${rowClass}>`;
             txt += `<td><input type="checkbox" ${isChecked} onchange="taskDone(${obj.id},this)"></td>`;
             txt += `<td>${obj.text}</td>`;
-            txt += `<td>${obj.category_id}</td>`;
+            txt += `<td>${catName}</td>`;
             txt += `<td><button onclick="deleteTask(${obj.id})">🗑️</button></td>`;
             txt += `<td><button onclick="taskToEdit(${obj.id})">✏️</button></td>`;
             txt += "</tr>";
@@ -52,4 +74,5 @@ async function taskDone(id, elm) {
     }
 } 
 
+getCategories();
 getTasks();
