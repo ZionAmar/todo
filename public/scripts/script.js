995112b7b6fc43fa6874735ae4e5d56eb633f2a9
Zion <amazion24@outlook.com>
2026-01-02 09:50:45 +0200

    הוספנו טבלה להצגת המשימות בדף

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,41 @@
 let greating = "Hello ";
 greating += localStorage.getItem('name');
 document.getElementById('greating').innerHTML = greating;
+
+async function getTasks() {
+    try {
+        let response = await fetch('/tasks');
+        if (response.status == 401) {
+            window.location.href = '/login';
+            return;
+        }
+        let data = await response.json();
+        if (response.status == 400) {
+            alert(data.message);
+            return;
+        }
+        createTable(data);
+    } catch (err) {
+        alert(err)
+    }
+}
+
+function createTable(data) {
+    let txt = "";
+    for (obj of data) {
+        if (obj) {
+            let isChecked = obj.is_done ? "checked" : "";
+            let rowClass = obj.is_done ? "class='rowClass'" : "";
+            txt += `<tr ${rowClass}>`;
+            txt += `<td><input type="checkbox" ${isChecked} onchange="taskDone(${obj.id},this)"></td>`;
+            txt += `<td>${obj.text}</td>`;
+            txt += `<td>${obj.category_id}</td>`;
+            txt += `<td><button onclick="deleteTask(${obj.id})">🗑️</button></td>`;
+            txt += `<td><button onclick="taskToEdit(${obj.id})">✏️</button></td>`;
+            txt += "</tr>";
+        }
+    }
+    document.getElementById('myTable').innerHTML = txt;
+}
+
+getTasks();
